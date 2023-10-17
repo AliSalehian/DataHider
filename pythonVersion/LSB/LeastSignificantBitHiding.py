4bb9b653d6b2d05e20484e9292a56bed9ee208a7
AliSalehian <ali.salehian77@gmail.com>
2023-10-17 19:18:08 +0330

    implement unhide part of LSB approach with python. I test it and it work properly

--- pythonVersion/LSB/LeastSignificantBitHiding.py
+++ pythonVersion/LSB/LeastSignificantBitHiding.py
@@ -5,6 +5,7 @@ import numpy as np
 from collections import deque
 
 
+# TODO: add type hint and document
 def encrypt(message):
     key = get_random_bytes(16)
     cipher = AES.new(key, AES.MODE_EAX)
@@ -13,6 +14,7 @@ def encrypt(message):
     return cipher_text, tag, nonce, key
 
 
+# TODO: add type hint and document
 def get_bit_array(number):
     result = [1 if digit == "1" else 0 for digit in bin(number)[2:]]
     result = deque(result)
@@ -21,6 +23,7 @@ def get_bit_array(number):
     return list(result)
 
 
+# TODO: add type hint and document
 def hide(key, tag, nonce, message, image_path):
     i, j, k = 0, 0, 0
     message += b"xyzXYZ"
@@ -30,7 +33,8 @@ def hide(key, tag, nonce, message, image_path):
         for element in part:
             bit_array = get_bit_array(element)
             for bit_value in bit_array:
-                print(f"i: {i}, j: {j} and k: {k}")
+                # TODO: put it in log
+                # print(f"i: {i}, j: {j} and k: {k}")
                 if bit_value == 0 and image_matrix[i, j, k] % 2 == 1:
                     image_matrix[i, j, k] += 1
                 elif bit_value == 1 and image_matrix[i, j, k] % 2 == 0:
@@ -54,4 +58,4 @@ if __name__ == "__main__":
         f"length of key: {len(key)}, length of tag: {len(tag)}, length of nonce: {len(nonce)} and length of cipher text: {len(cipher_text)}"
     )
     result_image = hide(key, tag, nonce, cipher_text, "test.jpg")
-    result_image.save("hasan.jpg")
+    result_image.save("hasan.png")
---
+++ pythonVersion/LSB/LeastSignificantBitUnhiding.py
@@ -0,0 +1,91 @@
+from Crypto.Cipher import AES
+from PIL import Image
+import numpy as np
+
+
+# TODO: add type hint and document
+def derive_information(image_path):
+    image = Image.open(image_path)
+    image_matrix = np.array(image)
+    i, j, k = 0, 0, 0
+    key, tag, nonce, cipher_text = None, None, None, None
+    temp = []
+    last_char_in_pattern = ""
+    fetching_cipher_text = False
+    while True:
+        temp_byte = []
+        while len(temp_byte) != 8:
+            if image_matrix[i, j, k] % 2 == 0:
+                temp_byte.append(0)
+            else:
+                temp_byte.append(1)
+            k += 1
+            if k >= image_matrix.shape[2]:
+                k = 0
+                j += 1
+            if j >= image_matrix.shape[1]:
+                j = 0
+                i += 1
+        decimal = 0
+        for bit in temp_byte:
+            decimal = (decimal << 1) | bit
+        temp.append(decimal)
+        if len(temp) == 16 and not fetching_cipher_text:
+            if key is None:
+                key = bytes(temp)
+            elif tag is None:
+                tag = bytes(temp)
+            elif nonce is None:
+                nonce = bytes(temp)
+                fetching_cipher_text = True
+            temp.clear()
+        if fetching_cipher_text:
+            if last_char_in_pattern == "":
+                if decimal == 120:
+                    last_char_in_pattern = "x"
+                    continue
+            elif last_char_in_pattern == "x":
+                if decimal == 121:
+                    last_char_in_pattern = "y"
+                    continue
+                else:
+                    last_char_in_pattern = ""
+            elif last_char_in_pattern == "y":
+                if decimal == 122:
+                    last_char_in_pattern = "z"
+                    continue
+                else:
+                    last_char_in_pattern = ""
+            elif last_char_in_pattern == "z":
+                if decimal == 88:
+                    last_char_in_pattern = "X"
+                    continue
+                else:
+                    last_char_in_pattern = ""
+            elif last_char_in_pattern == "X":
+                if decimal == 89:
+                    last_char_in_pattern = "Y"
+                    continue
+                else:
+                    last_char_in_pattern = ""
+            elif last_char_in_pattern == "Y":
+                if decimal == 90:
+                    cipher_text = bytes(temp)
+                    break
+    # TODO: put it in log
+    # print(
+    #     f"key: [{key}], tag: [{tag}], nonce: [{nonce}] and cipherText: [{cipher_text}]"
+    # )
+    return key, tag, nonce, cipher_text
+
+
+# TODO: add type hint and document
+def decrypt(key, tag, nonce, cipher_text):
+    cipher = AES.new(key, AES.MODE_EAX, nonce)
+    plaint_text = cipher.decrypt_and_verify(cipher_text, tag)
+    return plaint_text
+
+
+if __name__ == "__main__":
+    key, tag, nonce, cipher_text = derive_information("hasan.png")
+    print(decrypt(key, tag, nonce, cipher_text[:-6]))
